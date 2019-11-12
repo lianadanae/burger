a5ed1bd9e13f75314eee2e63abed4882b3bad8e7
lianadanae <lianabette@gmail.com>
2019-11-12 16:13:20 -0500

    updated burger.js

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+// Import the ORM to create functions that will interact with the database
+const orm = require('../config/orm.js');
+
+// Create burger object
+const burger = {
+  // Select all burger table entries
+  selectAll: function(cb) {
+    orm.selectAll('burgers', function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+};
+
+// Export the database functions for the controller (burgers_controller.js)
+module.exports = burger;
