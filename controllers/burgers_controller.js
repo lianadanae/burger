93f714e8d69f47e21002cf39ae31009f45acdfd6
lianadanae <lianabette@gmail.com>
2019-11-15 15:07:38 -0500

    fixed formatting

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,8 +6,8 @@ const router = express.Router();
 const burger = require('../models/burger.js');
 
 // Create all our routes and set up logic within those routes where required.
-router.get('/', function(req, res) {
-  burger.selectAll(function(data) {
+router.get('/', function (req, res) {
+  burger.selectAll(function (data) {
     const hbsObject = {
       burgers: data,
     };
@@ -16,33 +16,34 @@ router.get('/', function(req, res) {
   });
 });
 
-router.post('/api/burgers', function(req, res) {
+router.post('/api/burgers', function (req, res) {
   burger.insertOne(
-      ['burger_name'],
-      [req.body.burger_name],
-      function(result) {
-        // Send back the ID of the new quote
-        res.json({id: result.insertId});
+    ['burger_name'],
+    [req.body.burger_name],
+    function (result) {
+      // Send back the ID of the new quote
+      res.json({
+        id: result.insertId
       });
+    });
 });
 
-router.put('/api/burgers/:id', function(req, res) {
+router.put('/api/burgers/:id', function (req, res) {
   const condition = 'id = ' + req.params.id;
 
   console.log('condition', condition);
 
-  burger.updateOne(
-      {
-        devoured: true
-      },
-      condition,
-      function(result) {
-        if (result.changedRows === 0) {
+  burger.updateOne({
+      devoured: true
+    },
+    condition,
+    function (result) {
+      if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
-      },
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    },
   );
 });
 
