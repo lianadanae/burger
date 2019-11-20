c3a711dc48d86b73df1060f557495767aca71956
lianadanae <lianabette@gmail.com>
2019-11-19 19:19:05 -0500

    fixed password

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ connection = mysql.createConnection(process.env.JAWSDB_URL);
     port: 3306,
     host: 'localhost',
     user: 'root',
-    password: 'ChowDa2019!',
+    password: '',
     database: 'burgers_db'
   })
 };
